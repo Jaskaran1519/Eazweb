2ef8f01a91a5bb8932653e2ecf7dca4873ce8eb0
Jaskaran1519 <jaskaran1519@gmail.com>
2023-11-02 21:28:26 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,24 @@
+function sendMail() {
+    let params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      mobile: document.getElementById("mobile").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_w50tomj";
+    const templateID = "template_0c4h4b9";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("mobile").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+}

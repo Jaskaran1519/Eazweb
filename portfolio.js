b334f68139d7ab5c3222f9c0c61f9c3c49795a81
Jaskaran1519 <jaskaran1519@gmail.com>
2023-11-23 18:45:49 +0530

    contact

--- portfolio.js
+++ portfolio.js
@@ -1,29 +1,28 @@
-
-
 function sendMail() {
-    let params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      mobile: document.getElementById("mobile").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_w50tomj";
-    const templateID = "template_0c4h4b9";
-  
-      emailjs.send(serviceID, templateID, params)
-      .then(res=>{
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("mobile").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-  
-      })
-      .catch(err=>console.log(err));
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    mobile: document.getElementById("mobile").value,
+    message: document.getElementById("message").value,
+  };
 
-}                     
+  const serviceID = "service_w50tomj";
+  const templateID = "template_0c4h4b9";
 
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("mobile").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
 
-
+      let contactContent = document.getElementById("contact-form");
+      contactContent.innerHTML =
+        '<div style="width: 300px; height: 300px; color:green; padding:20px; margin: 0 auto; border-radius: 50%; border: 5px solid green; text-align: center;">' +
+        '<svg id="tick-image" width="200" height="200" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 12L10 22l-4-4" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>' + '<p>Message sent</p>'
+        '</div>';
+    })
+    .catch((err) => console.log(err));
+}
